0e7b09cae911f2e42cf6ff056d973a7e87dca698
arbid2544 <mrarbid.m@ku.th>
2021-02-08 17:35:39 +0700

    Initial commit

---
+++ 111_lab2-1.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,sum;
+    scanf("%d\n%d\n%d",&a,&b,&b);
+    sum=a+b+c;
+    if(sum>=80)
+        printf("A");
+    else if(sum>=75)
+        printf("B+");
+    else if(sum>=70)
+        printf("B");
+    else if(sum>=65)
+        printf("C+");
+    else if(sum>=60)
+        printf("C");
+    else if(sum>=55)
+        printf("D");
+    else if(sum>=50)
+        printf("D+");
+    else
+        printf("F");
+    return 0;
+}
